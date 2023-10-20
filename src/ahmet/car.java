5ce952f4f1c46637e7fc97a1115a10271c14fa51
Ahmet Faruk ÇATLAR <ahmetfaruk411@gmail.com>
2023-10-20 14:34:22 +0300

    Sınıf Tanımları

--- src/ahmet/car.java
+++ src/ahmet/car.java
@@ -1,5 +1,29 @@
 package ahmet;
 
-public class car {
+class car {
+	// nitelikler
+	String type;
+	String model;
+	String color;
+	int speed;
+	int speedLimit = 180;
+
+	void increaseSpeed(int increment) {
+		if ((speed + increment) < speedLimit) {
+			speed += increment;
+		}
+	}
+
+	void decreaseSpeed(int decrease) {
+		if (speed > 0) {
+			speed -= decrease;
+
+		}
+	}
+
+	void printSpeed() {
+		System.out.println("Hızınız: " + speed);
+
+	}
 
 }
--- src/ahmet/nesnes_yonelimli_programlama.java
+++ src/ahmet/nesnes_yonelimli_programlama.java
@@ -3,8 +3,8 @@ package ahmet;
 public class nesnes_yonelimli_programlama {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 }
