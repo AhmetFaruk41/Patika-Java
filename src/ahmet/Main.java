650188223908baf4d9fd0c912951afff2375e64a
Ahmet Faruk ÇATLAR <ahmetfaruk411@gmail.com>
2023-10-23 13:34:22 +0300

    Nesne oluşturma ve sınıf metotlari

--- src/ahmet/Main.java
+++ src/ahmet/Main.java
@@ -3,28 +3,17 @@ package ahmet;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		car audi = new car();
-		audi.speed = 130;
-		audi.model= "Rs7";
-		audi.increaseSpeed(20);
-		
-		System.out.println(audi.model +" hızı: "+ audi.speed);
-		
-		car bmw = new car();
-		bmw.model= "bmw";
-		bmw.speed= 140;
-		
-		System.out.println(bmw.model +" hızı: "+ bmw.speed);
 
-		car mercedes = new car();
-		mercedes.model= "cls";
-		mercedes.speed= 150;
-		
-		System.out.println(mercedes.model +" hızı: "+ mercedes.speed);
+		car audi = new car("rs7", 120, "blue");
+		audi.printİnfo();
 
-		
-		
+		// System.out.println(audi.model + " hızı: " + audi.speed);
+
+		car bmw = new car("m8", 130, "black");
+		bmw.printİnfo();
+
+		car mercedes = new car("G63", 140, "white");
+		mercedes.printİnfo(); 
 		
 	}
 
--- src/ahmet/car.java
+++ src/ahmet/car.java
@@ -8,6 +8,18 @@ class car {
 	int speed;
 	int speedLimit = 180;
 
+	
+	car(String audi,int speed,String color) {
+		this.model=audi;
+		this.speed=speed;
+		this.color=color;
+		this.type ="sedan";
+		this.speedLimit=180;
+		
+		}
+	
+	
+
 	void increaseSpeed(int increment) {
 		if ((speed + increment) < speedLimit) {
 			speed += increment;
@@ -25,5 +37,13 @@ class car {
 		System.out.println("Hızınız: " + speed);
 
 	}
+	void printİnfo() {
+		System.out.println("Model\t:"+this.model);
+		System.out.println("Color\t:"+this.color);
+		System.out.println("Speed\t:"+this.speed);
+		System.out.println("Type\t:"+this.type);
+
+	}
+	
 
 }
